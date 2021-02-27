54e2084442b76dd511f47ea4521463ec8f2bae59
Mohammad Reza Keikaavousi <keikaavousi@gmail.com>
2021-02-27 18:30:25 +0330

    load svg images from file content

---
+++ resources/views/components/tabs.blade.php
@@ -0,0 +1,165 @@
+<div class="home-network__tab-container">
+    <ul class="home-network__tabs">
+        <li data-id="tab1" class="home-network__tab tab selected">ابریار کوچ ابری</li>
+        <li data-id="tab2" class="home-network__tab tab">ابریار بازارچه</li>
+        <li data-id="tab3" class="home-network__tab tab">ابریار فنی</li>
+        <li data-id="tab4" class="home-network__tab tab">ابریار فروش</li>
+        <li data-id="tab5" class="home-network__tab tab">ابریار آموزش</li>
+    </ul>
+
+    <div id="tab1" class="home-network__tab-panel tab-panel">
+        <h3 class="home-network__tab-title">
+            ابریار کوچ ابری کیست؟
+        </h3>
+        <p class="home-network__tab-content">ابریاران کوچ ابری می‌توانند با ارایه‌ی خدمات فنی و مشاوره‌ای،
+            برگزاری جلسات فنی و طراحی راهکارهای ابری
+            یکپارچه، مشتریان آروان را در مهاجرت به زیرساخت‌های ابر آروان یاری کنند</p>
+        <h3 class="home-network__tab-title">سطح‌بندی و انتظارت از ابریارهای کوچ ابری</h3>
+
+
+        <div class="home-network__inner-tab-container">
+            <div class="home-network__inner-tabs">
+                <div class="home-network__inner-tab-wrapper">
+                    <div data-id="inner-tab1" class="home-network__inner-tab tab selected">سطح ۱</div>
+                    <div id="inner-tab1" class="home-network__inner-tab-panel tab-panel">
+                        <div class="row home-network__inner-tab-panel-row">
+
+                            <div class="home-network__box home-network__box--fill">
+                                <div class="home-network__box-icon">
+                                    <?= file_get_contents('img/medal.svg') ?>
+                                </div>
+                                <h3 class="home-network__box-title">عنوان رتبه</h3>
+                                <h4 class="home-network__box-subtitle">قهرمان</h4>
+                            </div>
+
+                            <div class="home-network__box home-network__box--fill">
+                                <div class="home-network__box-icon">
+                                    <?= file_get_contents('img/transcript.svg') ?>
+                                </div>
+                                <h3 class="home-network__box-title">سابقه</h3>
+                                <h4 class="home-network__box-subtitle">۱۰ سال</h4>
+                            </div>
+
+                            <div class="home-network__box home-network__box--fill">
+                                <div class="home-network__box-icon">
+                                    <?= file_get_contents('img/users.svg') ?>
+                                </div>
+                                <h3 class="home-network__box-title">تعداد نفرات</h3>
+                                <h4 class="home-network__box-subtitle">۷ نفر فنی - ۳ نفر بیزنسی</h4>
+                            </div>
+                        </div>
+
+                        <div class="row home-network__inner-tab-panel-row">
+                            <div class="home-network__box home-network__box--outline">
+                                <div class="home-network__box-icon">
+                                    <?= file_get_contents('img/paper.svg') ?>
+                                </div>
+                                <div class="home-network__box-description-wrapper">
+                                    <h3 class="home-network__box-title">نیازمندی‌ها</h3>
+                                    <ul class="home-network__box-list">
+                                        <li class="home-network__box-item">امکان ارائه پشتیبانی 24/7</li>
+                                        <li class="home-network__box-item">لایه یک و دو</li>
+                                        <li class="home-network__box-item">وبسایت</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="home-network__box home-network__box--outline">
+                                <div class="home-network__box-icon">
+                                    <?= file_get_contents('img/clipboard.svg') ?>
+                                </div>
+                                <div class="home-network__box-description-wrapper">
+                                    <h3 class="home-network__box-title">انتظارات</h3>
+                                    <p class="home-network__box-description">
+                                        برگزاری حداقل 5 رویداد سالیانه در زمینه تکنولوژی
+                                        ابری و فرآیندهای کوچ ابری
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row home-network__inner-tab-panel-row">
+                            <div class="home-network__box home-network__box--outline">
+                                <div class="home-network__box-icon">
+                                    <?= file_get_contents('img/pen.svg') ?>
+                                </div>
+                                <div class="home-network__box-description-wrapper">
+                                    <h3 class="home-network__box-title">توانمندی ها</h3>
+                                    <ul class="home-network__box-list">
+                                        <li class="home-network__box-item">تسلط کامل به ساختارهای Container و
+                                            سیستم‌های
+                                            Orchestration مانند Docker و Kubernetes تسلط بر فرایند های CI/CD
+                                        </li>
+                                        <li class="home-network__box-item">تسلط بر حداقل یک زبان اسکریپت نویسی
+                                            (Python,
+                                            Bash)</li>
+                                        <li class="home-network__box-item">مسلط به سیستم‌عامل، شبکه و نگهداری
+                                            زیرساخت‌های نرم‌افزاری در لود بالا </li>
+                                        <li class="home-network__box-item">بهبود و طراحی معماری زیرساخت سرویس‌ها
+                                            برای
+                                            پیاده‌سازی در لود بالا و درک عمیق از TCP/IP </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="home-network__inner-tab-wrapper">
+                    <div data-id="inner-tab2" class="home-network__inner-tab tab">سطح ۲</div>
+                    <div id="inner-tab2" class="home-network__inner-tab-panel tab-panel hidden">
+                        محتوای سطح ۲
+                    </div>
+                </div>
+
+                <div class="home-network__inner-tab-wrapper">
+                    <div data-id="inner-tab3" class="home-network__inner-tab tab">سطح ۳</div>
+                    <div id="inner-tab3" class="home-network__inner-tab-panel tab-panel hidden">
+                        محتوای سطح ۳
+                    </div>
+                </div>
+
+                <div class="home-network__inner-tab-wrapper">
+                    <div data-id="inner-tab4" class="home-network__inner-tab tab">سطح ۴</div>
+                    <div id="inner-tab4" class="home-network__inner-tab-panel tab-panel hidden">
+                        محتوای سطح ۴
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="tab2" class="home-network__tab-panel tab-panel hidden">
+        <h3 class="home-network__tab-title">
+            ابریار بازارچه کیست؟
+        </h3>
+        <p class="home-network__tab-content">ابریاران بازارچه ابری می‌توانند با ارایه‌ی خدمات فنی و مشاوره‌ای،
+            برگزاری جلسات فنی و طراحی راهکارهای ابری
+            یکپارچه، مشتریان آروان را در مهاجرت به زیرساخت‌های ابر آروان یاری کنند</p>
+
+    </div>
+    <div id="tab3" class="home-network__tab-panel tab-panel hidden">
+        <h3 class="home-network__tab-title">
+            ابریار فنی کیست؟
+        </h3>
+        <p class="home-network__tab-content">ابریاران فنی ابری می‌توانند با ارایه‌ی خدمات فنی و مشاوره‌ای،
+            برگزاری جلسات فنی و طراحی راهکارهای ابری
+            یکپارچه، مشتریان آروان را در مهاجرت به زیرساخت‌های ابر آروان یاری کنند</p>
+
+    </div>
+    <div id="tab4" class="home-network__tab-panel tab-panel hidden">
+        <h3 class="home-network__tab-title">
+            ابریار فروش کیست؟
+        </h3>
+        <p class="home-network__tab-content">ابریاران فروش ابری می‌توانند با ارایه‌ی خدمات فنی و مشاوره‌ای،
+            برگزاری جلسات فنی و طراحی راهکارهای ابری
+            یکپارچه، مشتریان آروان را در مهاجرت به زیرساخت‌های ابر آروان یاری کنند</p>
+
+    </div>
+    <div id="tab5" class="home-network__tab-panel tab-panel hidden">
+        <h3 class="home-network__tab-title">
+            ابریار آموزش کیست؟
+        </h3>
+        <p class="home-network__tab-content">ابریاران آموزش ابری می‌توانند با ارایه‌ی خدمات فنی و مشاوره‌ای،
+            برگزاری جلسات فنی و طراحی راهکارهای ابری
+            یکپارچه، مشتریان آروان را در مهاجرت به زیرساخت‌های ابر آروان یاری کنند</p>
+
+    </div>
+</div>
